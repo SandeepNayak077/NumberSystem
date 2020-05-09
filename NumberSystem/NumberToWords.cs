22c6302a3edb4305bbbe58f27eec529cffeed776
Sandeep Nayak <sandeep.nayak077@gmail.com>
2020-05-09 21:44:26 +0530

    C0006: Implemented Convert() method which spells given number

--- NumberSystem/NumberToWords.cs
+++ NumberSystem/NumberToWords.cs
@@ -8,11 +8,66 @@ namespace NumberSystem
 {
     public class NumberToWords
     {
+        const long MILLION = 1000000;
+        const long THOUSAND = 1000;
+        const long HUNDRED = 100;
+
+        static readonly string[] _smallNumbers = new string[]{ "Zero", "One", "Two", "Three", "Four", "Five",
+                                                               "Six", "Seven", "Eight", "Nine", "Ten",
+                                                               "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
+                                                               "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+                                                             };
+
+        static readonly string[] _tens = new string[] { "Zero", "Ten", "Twenty", "Thirty", "Forty",
+                                                        "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
         public static string Convert(long number)
         {
             StringBuilder words = new StringBuilder();
 
-            //TODO
+            if (number == 0)
+            {
+                return _smallNumbers[0];
+            }
+
+            if (number / MILLION > 0)
+            {
+                words.Append(Convert(number / MILLION) + " Million, ");
+                number %= MILLION;
+            }
+
+            if (number / THOUSAND > 0)
+            {
+                words.Append(Convert(number / THOUSAND) + " Thousand, ");
+                number %= THOUSAND;
+            }
+
+            if (number / HUNDRED > 0)
+            {
+                words.Append(Convert(number / HUNDRED) + " Hundred ");
+                number %= HUNDRED;
+            }
+
+            if (number > 0)
+            {
+                if (words.Length > 0)
+                {
+                    words.Append("and ");
+                }
+
+                if (number < 20)
+                {
+                    words.Append(_smallNumbers[number]);
+                }
+                else
+                {
+                    words.Append(_tens[number / 10]);
+                    if (number % 10 > 0)
+                    {
+                        words.Append("-" + _smallNumbers[number % 10]);
+                    }
+                }
+            }
 
             return words.ToString();
         }
