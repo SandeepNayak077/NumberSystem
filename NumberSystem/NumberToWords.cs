0c138916eb7bd57a8a846b41a52c0727fd26c752
Sandeep Nayak <sandeep.nayak077@gmail.com>
2020-05-09 21:14:15 +0530

    C0003: Added NumberSystem project to implement NumberToWords.Convert() method

---
+++ NumberSystem/NumberToWords.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSystem
+{
+    public class NumberToWords
+    {
+        public static string Convert(long number)
+        {
+            StringBuilder words = new StringBuilder();
+
+            //TODO
+
+            return words.ToString();
+        }
+    }
+}
