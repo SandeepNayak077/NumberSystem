165b2fc3aff32caa827593be1d5de6c69c5e3dc9
Sandeep Nayak <sandeep.nayak077@gmail.com>
2020-05-09 21:06:42 +0530

    C0002: Created basic test case

---
+++ TestNumberSystem/UT_NumberToWords.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestNumberSystem
+{
+    [TestClass]
+    public class UT_NumberToWords
+    {
+        [TestMethod]
+        public void T0001_BasicTest()
+        {
+            string actualResult = NumberToWords.Convert(1234);
+            string expectedResult = "One Thousand, Two Hundred and Thirty-Four";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
